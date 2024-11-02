fc5c7cf2f0f69f3b996147ebb2852a3ea74b5256
ntdat02092002 <datnt030902@gmail.com>
2024-11-02 22:20:33 +0700

    update data source

--- pinecone_data.py
+++ pinecone_data.py
@@ -1,4 +1,7 @@
 import os
+import requests
+
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 from pinecone.grpc import PineconeGRPC as Pinecone
@@ -10,40 +13,77 @@ from pinecone import ServerlessSpec
 from langchain_core.documents import Document
 from langchain_huggingface import HuggingFaceEmbeddings, HuggingFaceEndpoint
 from langchain_pinecone import PineconeVectorStore
+from langchain_text_splitters import HTMLHeaderTextSplitter
 
 
-"""Test Data"""
+"""Get Data"""
 
-docs = [
-    Document(
-        page_content="Elise: Passive spiderlings now always regen when resummoned and jump on Q cast. Human E now reveals stealthed units and can now be redirected via flash. Spider E buff now applies on cast and can be recast to immediately drop back. R cooldown decreased.",
-        metadata={"type": "champion"},
-    ),
-    Document(
-        page_content="K'Sante: Base Attack Range reduced. All abilities adjusted.",
-        metadata={"type": "champion"},
-    ),
-    Document(
-        page_content="Tristana: Basic Attack Range, Damage, and Attack Speed Ratio increased. AD growth, HP Regen growth, and Armor growth decreased. Passive range decreased at higher levels. Q Attack Speed increased. W slow decreased and duration changed to a flat value. W AD ratio added. E damage profile reworked to value crit. R cooldown changed to a flat value. R stun added. R damage profile reworked to account for AD ratio.",
-        metadata={"type": "champion"},
-    ),
-    Document(
-        page_content="Vladimir: W healing effectiveness reduced against minions.",
-        metadata={"type": "champion"},
-    ),
-    Document(
-        page_content="Components: Our goal with the component changes is to more accurately hit their true gold-to-stat values. Early resources and regeneration is still intentionally overpriced in order to keep those components from overtaking the laning phase, but they shouldn't feel like such large downgrades when they're part of later game builds.",
-        metadata={"type": "item"},
-    ),
-    Document(
-        page_content="Dagger: Total Cost: 300 => 250\nFaerie Charm: Total Cost: 250 => 200\nSapphire Crystal:Total Cost: 350 => 300, Mana: 250 => 300",
-        metadata={"type": "item"},
-    ),
-    Document(
-        page_content="Fighter Items: We rebalanced fighter items with the three classes in mind: skirmishers, who need to be highly lethal and require finesse to find success, divers, who need to get through the front line to access their weaker targets, and juggernauts, who should be tanky enough to hit whoever's in front of them. With these goals in mind, we focused on balancing item power budget between durability—which there's a lot of on fighter items—and repeatable damage effects. We also wanted to ensure that some resist-heavy tank items remain solid picks for when fighter players need to take up tank duties to counter enemy team comps.",
-        metadata={"type": "item"},
-    ),
+# Lấy nội dung trang HTML
+url = "https://www.leagueoflegends.com/en-us/news/game-updates/patch-14-21-notes/"
+response = requests.get(url)
+if response.status_code == 200:
+    page_content = response.content
+else:
+    raise f"[404] - URL not found - {url}"
+
+# Parse HTML và lấy phần có id="patch notes"
+soup = BeautifulSoup(page_content, "html.parser")
+patch_section = soup.find(id="patch-notes-container")
+
+# Danh sách các thẻ chỉ có tác dụng định dạng mà không ảnh hưởng tới nội dung
+formatting_tags = ["strong", "em", "u", "b", "i", "s"]
+
+# Nối tất cả nội dung của thẻ và thẻ con vào một chuỗi, giữ nguyên HTML
+full_html_content = ""
+if patch_section:
+    for child in patch_section.find_all(recursive=False):
+        # Chèn \n sau phần văn bản nếu có nội dung thực sự và không thuộc thẻ định dạng
+        for elem in child.find_all(text=True):
+            parent_tag = elem.parent.name
+            if elem.strip():  # Kiểm tra chuỗi có nội dung thực sự
+                # Chỉ chèn \n nếu thẻ cha không nằm trong danh sách các thẻ định dạng
+                if parent_tag not in formatting_tags:
+                    elem.replace_with(elem + "\\n")
+        full_html_content += str(child)  # Giữ nguyên HTML của các thẻ con
+else:
+    raise "cannot find <id='patch notes'> element"
+
+
+"""Parse data to docs"""
+
+headers_to_split_on = [
+    ("h1", "h1"),
+    ("h2", "h2"),
+    ("h3", "h3"),
+    ("h4", "h4"),
+    ("h5", "h5"),
 ]
+
+splitter = HTMLHeaderTextSplitter(headers_to_split_on)
+
+# Tách nội dung thành các đoạn theo các thẻ tiêu đề
+docs = splitter.split_text(full_html_content)
+
+for doc in docs:
+    # first doc - overview patch with no header (no metadata)
+    if not doc.metadata:
+        doc.metadata = {"type": "overview"}
+        continue
+
+    page_content = ""
+    type = "other"
+    for key, value in doc.metadata.items():
+        value = value.strip("\\n").strip("\n") # strip \n - the addition when process by beautifulsoup
+        page_content += f"<{key}>{value}<{key}>\n"
+        if key == "h2" and value == "Champions":
+            type = "champion"
+        elif key == "h2" and value == "Items":
+            type = "item"
+            
+    page_content += doc.page_content
+
+    doc.page_content = page_content
+    doc.metadata = {"type": type}
 
 
 """Load data to pinecone"""
@@ -67,7 +107,7 @@ pc_index = pc.Index(PINECONE_INDEX_NAME)
 print(pc_index.describe_index_stats())
 
 embeddings = HuggingFaceEmbeddings()
-namespace = "test"
+namespace = "lol-patch"
 
 try:
     pc_index.delete(namespace=namespace, delete_all=True)
